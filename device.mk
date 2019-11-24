9bad8d4e8e4b4570af4e352f2ec339951140a6de
LuK1337 <priv.luk@gmail.com>
2019-11-24 20:25:39 +0100

    garnet: overlay-lineage: Add reboot to fastbootd
    Change-Id: I733b4d29129f85b92222e292af46602310b9f29f

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlayGarnet \
     FrameworkOverlayGarnet \
     FrameworkOverlayGarnetEsim \
+    LineageSDKOverlayGarnet \
     SettingsOverlayGarnet \
     SettingsProviderOverlayGarnetPoco \
     SettingsProviderOverlayGarnetRedmi \
