3b37e6b37e771dfea98af5fb344889abb011707a
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-05 00:36:20 +0530

    garnet: Import overlays
     * esim overlays have been left out for now
    
    Change-Id: I61aee441ba741224e8e9e91a62be1adcd705899b

--- device.mk
+++ device.mk
@@ -10,6 +10,14 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayGarnet \
+    FrameworkOverlayGarnet \
+    SystemUIOverlayGarnet \
+    TelephonyOverlayGarnet \
+    WifiOverlayGarnet
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
