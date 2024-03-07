cce3b218cebc4c415e6e0188c960526470bba35d
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-07 18:19:47 +0530

    garnet: Import esim overlays
    Change-Id: If5e8fc91b29045ec8727d86ee97df96567197a5b

--- device.mk
+++ device.mk
@@ -14,6 +14,7 @@ $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayGarnet \
     FrameworkOverlayGarnet \
+    FrameworkOverlayGarnetEsim \
     SettingsOverlayGarnet \
     SettingsProviderOverlayGarnetPoco \
     SettingsProviderOverlayGarnetRedmi \
