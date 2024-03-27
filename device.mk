823cda47d6433dcfd8b9025263a46358a0331672
Michael Bestas <mkbestas@lineageos.org>
2024-03-28 00:06:56 +0200

    garnet: overlay: Enable battery cycle count
    Change-Id: I92bc8363e1c9e2ad54c29a0a30dce992e25f697d

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@ PRODUCT_PACKAGES += \
     FrameworkOverlayGarnet \
     FrameworkOverlayGarnetEsim \
     LineageSDKOverlayGarnet \
+    LineageSettingsOverlayGarnet \
     SettingsOverlayGarnet \
     SettingsProviderOverlayGarnetPoco \
     SettingsProviderOverlayGarnetRedmi \
