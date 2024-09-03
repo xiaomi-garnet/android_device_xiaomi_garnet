62309f7735d69591b633318ca77e9589cd5b0685
UnmoveD <unmoved2121@gmail.com>
2024-09-03 15:13:57 +0200

    garnet: Initial device tree

---
+++ lineage_garnet.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from mondrian device
+$(call inherit-product, device/xiaomi/garnet/device.mk)
+
+# Inherit from common lineage configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_garnet
+PRODUCT_DEVICE := garnet
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 2312DRA50G
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
